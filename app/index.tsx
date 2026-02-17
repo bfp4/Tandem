b09155b2344a8b9f97ad27e85dc192fe2e82aae4
bfp4 <generalarileverton@gmail.com>
2026-02-17 14:48:40 -0500

    Remove bad stuff

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        hello world, <Text style={styles.bold}>Huber</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
