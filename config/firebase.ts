1aa62775025c553e9faf8fc9732c32a180c442b5
bfp4 <generalarileverton@gmail.com>
2026-05-24 21:48:28 -0400

    Masked API keys

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,13 +6,13 @@ import { getReactNativePersistence } from '@firebase/auth/dist/rn';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7KcCm9e5HK_GSv9PKIUl4KijPCaXXxoI",
-  authDomain: "capstone-499-77660.firebaseapp.com",
-  projectId: "capstone-499-77660",
-  storageBucket: "capstone-499-77660.firebasestorage.app",
-  messagingSenderId: "985020817268",
-  appId: "1:985020817268:web:d921c8e09c3c98a19f9621",
-  measurementId: "G-JG04VRCVT4"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
